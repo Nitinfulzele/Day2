5616323367da1ce5fc8822089bd0da64e48ce265
Nitinfulzele <nitinfulzele99@gmail.com>
2022-09-28 18:40:55 +0530

    Single Digit Number

---
+++ SingleDigitNo.java
@@ -0,0 +1,16 @@
+
+public class SingleDigitNo {
+	
+public static void main(String [] args) {
+		
+		int num = 20;
+		
+		if(num < 10) {
+			System.out.println("Ten");
+		}
+		else {
+			System.out.println("Twenty");
+		}
+	}
+
+}
